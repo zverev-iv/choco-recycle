424d9f2f3269b0b8ca327bb30c733dfc03a414ff
Илья Зверев <izverev@logrocon.local>
2020-02-04 14:17:40 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# choco-recycle
+Chocolatey package for recycle http://ss64.net/westlake/xp/index.html
+
+original text of license:
+
+```txt
+Unrestricted License
+
+Full copyright is retained by Frank P. Westlake but all entities,
+persons, and other life forms are granted unrestricted usage, copying,
+distribution, and coloring.
+
+Frank P.Westlake
+```
---
+++ recycle/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'http://ss64.net/westlake/xp/recycle.zip'
+  softwareName  = 'recycle*'
+  checksum      = '69b4cffea621b10f6a4afc9456a0b97a0f57428d286edac888fadcb4b93ba626'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
